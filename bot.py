2ccf1d94117d09e0db8eb6c63e21955d322b116b
Bastian Perez <didier.bpp@gmail.com>
2021-09-06 23:50:35 -0300

    Soluciono bugs

--- bot.py
+++ bot.py
@@ -19,10 +19,12 @@ import sys
 class Bot():
     def __init__(self, *args, **kwargs ):
         options = Options()
-        # self.start_timer = time.time()
-        # options.add_argument('--headless')
+      
+        options.add_argument('--headless')
+        options.add_argument('--ignore-certificate-errors-spki-list') 
+        options.add_argument('log-level=3')
         options.add_argument('window-size=1920x1080')
-        # options.add_argument("--incognito")
+        options.add_argument("--incognito")
         # options.add_argument("--disable-gpu")
         # options.add_argument("-disable-software-rasterizer")
         self.driver = webdriver.Chrome(ChromeDriverManager(log_level=0).install(),options=options)
@@ -30,7 +32,8 @@ class Bot():
         self.url = ''
         self.email_content = ''
         self.exists_discounts = False
-        self.discount = 40
+        self.exists_exception = False
+        self.discount = 70
         self.max_page_retry  = 0
         self.try_count = 3
         self.counter_page = 0
@@ -41,7 +44,6 @@ class Bot():
     def check_exists_inside_by_xpath(self,element,xpath):
         element_check = None
         try:
-          
             element_check = element.find_element_by_xpath(xpath)
         except NoSuchElementException:
             return element_check
@@ -53,39 +55,41 @@ class Bot():
         except (TimeoutException,NoSuchElementException):
             return False
         return True
-    def email_send(send,subject,content):
+    def email_send(self,subject,content):
         #The mail addresses and password
         sender_address = 'bbasti390@gmail.com'
         sender_pass = 'jdjtzycowlaawmgd'
         receivers_address = ['bastididierr@gmail.com','anaisacvdo@gmail.com']
         #Setup the MIME
-        msg = """
+        
+        html = """      
+                <html>
+                <body>
+                    <h1>Tu descuentos :) </h1>
+                    <table style="width:100%,border:.5px solid black">
+                        <tr>
+                            <th style="border:.5px solid black">MARCA</th>
+                            <th style="border:.5px solid black">Producto</th>
+                            <th style="border:.5px solid black">URL</th>
+                            <th style="border:.5px solid black">Precio</th>
+                            <th style="border:.5px solid black">Dcto</th>
                         
-                                <html>
-                                <body>
-                                    <h1>Tu descuentos :) </h1>
-                                    <table style="width:100%,border:.5px solid black">
-                                        <tr>
-                                            <th style="border:.5px solid black">MARCA</th>
-                                            <th style="border:.5px solid black">Producto</th>
-                                            <th style="border:.5px solid black">URL</th>
-                                            <th style="border:.5px solid black">Precio</th>
-                                            <th style="border:.5px solid black">imagen</th>
-                                        </tr>
-                                    {}
-                        
-                                    </table>
-                                    
-                                    <p>Regards,</p>
-                                <p>Me</p>
-                                </body>
-                                </html>
-                                """.format(content)
-        message = MIMEMultipart( "alternative", None, [ MIMEText(msg,'html')])
-        # message = MIMEMultipart( )
+                        </tr>
+                    {}
+        
+                    </table>
+                    
+                    <p>Por favor, antes de comprar el producto que te interese te recomiendo
+                    ver si el precio no esta inflado, en paginas como https://www.knasta.cl o https://www.solotodo.cl
+                    </p>
+                <p>Nos vemos!! :)</p>
+                </body>
+                </html>
+                """.format(content)
+        message = MIMEMultipart( "alternative", None, [ MIMEText(html,'html')])
         message['From'] = sender_address
-   
         message['Cco'] = ", ".join(receivers_address)
+
         datetime_hour_for_now =  datetime.timedelta(hours=4)
         date_now = "{:%d-%m-%Y %H:%M:%S}".format(datetime.datetime.now() - datetime_hour_for_now )
         subject_with_date = "{} {}".format(subject,date_now)
@@ -103,21 +107,24 @@ class Bot():
     def get_list_products_info(self,element_container):
         try:
             list_product = []
+            
             print("ESCANEO PRODUCTOS: ",len(element_container))
            
             self.wait.until(EC.element_to_be_clickable((By.XPATH, "//b[starts-with(@id, 'testId-pod-displaySubTitle')]")))
             self.wait.until(EC.element_to_be_clickable((By.XPATH, "//a[contains(@class, 'layout_grid-view')]")))
-            self.wait.until(EC.visibility_of_all_elements_located((By.XPATH, "//img[starts-with(@id, 'testId-pod-image')]")))
+
             self.wait.until(EC.element_to_be_clickable((By.XPATH, "//b[contains(@class, 'pod-title')]")))
             self.wait.until(EC.element_to_be_clickable((By.XPATH, "//span[starts-with(@class, 'copy10')]")))
             for product in element_container:
-                
+              
                 object_product = {}
                 # Nombre del producto
                 # print("INDEX PRODUCTOS: ",index)
         
                 # self.wait.until(EC.element_to_be_clickable((By.XPATH, "//b[starts-with(@id, 'testId-pod-displaySubTitle')]")))
+           
                 product_name_list = product.find_elements_by_xpath(".//b[starts-with(@id,'testId-pod-displaySubTitle')]")
+
                 # Obtener descuento del producto
                 # Hago un check para ver si existen descuentons en la pagina
                 # Si no existen regresa un array vacio
@@ -134,11 +141,9 @@ class Bot():
         
                 # Obtengo el link del producto
                 # self.wait.until(EC.element_to_be_clickable((By.XPATH, "//a[contains(@class, 'layout_grid-view')]")))
-                # self.driver.find_element_by_tag_name
+               
                 link = product.find_elements_by_xpath(".//a[contains(@class, 'layout_grid-view')]")
-                # TODO: VER POR QUE NO TOMA TODAS LAS IMAGNEES
-                
-                image_product = link[0].find_elements_by_xpath(".//img")
+    
                 # self.wait.until(EC.element_to_be_clickable((By.XPATH, "//b[contains(@class, 'pod-title')]")))
                 brand_product = product.find_elements_by_xpath(".//b[contains(@class, 'pod-title')]")
 
@@ -149,10 +154,7 @@ class Bot():
                 link_ref  = link[0].get_attribute('href')
                 
                 
-                if len(image_product) !=0:
-                    image_src = image_product[0].get_attribute('srcset')
-                else:
-                    image_src = ""
+        
 
                 brand = brand_product[0].text
                 if len(brand_product) != 0:
@@ -167,8 +169,10 @@ class Bot():
                                                 <td  style="border:.5px solid black">{}</td>
                                                 <td  style="border:.5px solid black"><a href={}>Link</a</td>
                                                 <td  style="border:.5px solid black">{}</td>
-                                                <td  style="border:.5px solid black"><img src="{}" width="100" height="100"></td>
-                                            </tr>""".format(brand,product_name,link_ref,price,image_src)
+                                                <td  style="border:.5px solid black">{}%</td>
+                                            </tr>""".format(brand,product_name,link_ref,price,dcto_bag)
+            # TODO: VER QUE HACE AL MOMENTO DE ENCONTRAR UN ERROR SI SIGUE NORMAN SIN EL BREAK
+        
             print("FINALIZO SCAN")
         
             
@@ -184,14 +188,15 @@ class Bot():
                     
             
         except (NoSuchElementException,MaxRetryError,TimeoutException,Exception) as error: 
-            self.url = self.driver.current_url
-            self.try_count -= 1
-            print("URL: ", self.url, "TRY COUNT: ",self.try_count)
-            print("EXCEPT: get_list_products_info")
             exception_type, exception_object, exception_traceback = sys.exc_info()
             filename = exception_traceback.tb_frame.f_code.co_filename
             line_number = exception_traceback.tb_lineno        
-            print(f"ExecptionType: {exception_type}\n ARCHIVO: {filename}\n LINEA:{ line_number } \n Object: {exception_object} \nERROR"  ,error)
+            print(f"ExecptionType: {exception_type}\n ARCHIVO: {filename}\n LINEA:{ line_number } \n ERROR: {exception_object}") 
+            self.url = self.driver.current_url
+            self.try_count -= 1
+            self.exists_exception = True
+            
+
             
     def for_products_container(self,max_page):
         try:
@@ -203,18 +208,25 @@ class Bot():
                     if self.counter_page == int(max_page):
                         print("LLEGO AL FINAL DE PAGINAS")
                         self.try_count = 0
+                    if self.try_count == 0:
+                        break
+                        
                     print("INDEX --> ",i )
                     # TODO: simular error en la pagina 2 para hacer un reeset y vuelva ejecutar el bot 
                     # abajo esta para sabeer que tipo de excep es
                     # TODO: luego borrar este if dejar e oslo el else
                     # if i ==1:
-                    #     self.wait.until(EC.presence_of_all_elements_located((By.XPATH, "//div[@data-pod='catalyst-pod']")))
+                    #     self.wait.until(EC.presence_of_all_elements_located((By.XPATH, "//div[@data-pod='catalyst-pod-----']")))
                     #     container_product = self.driver.find_elements_by_xpath( "//div[@data-pod='catalyst-pod']")
                     # else:
                     self.wait.until(EC.presence_of_all_elements_located((By.XPATH, "//div[@data-pod='catalyst-pod']")))
                     container_product = self.driver.find_elements_by_xpath( "//div[@data-pod='catalyst-pod']")
 
                     self.get_list_products_info(container_product)
+                    # Si existe una exception en get_list_products_info, esta 
+                    # se saldra en el if de a continuacion para reiniciar la pagina
+                    if self.exists_exception:
+                        break
                     if i != int(max_page) -1:
                         self.wait.until(EC.element_to_be_clickable((By.ID, "testId-pagination-bottom-arrow-right")))
                         # self.wait.until(EC.invisibility_of_element_located((By.ID, "kampyleInviteContainer")))
@@ -228,36 +240,23 @@ class Bot():
             exception_type, exception_object, exception_traceback = sys.exc_info()
             filename = exception_traceback.tb_frame.f_code.co_filename
             line_number = exception_traceback.tb_lineno        
-            print(f"ExecptionType: {exception_type}\n ARCHIVO: {filename}\n LINEA:{ line_number } \n ERROR: {exception_object}") 
-            print("URL: ", self.url, "TRY COUNT: ",self.try_count)
             self.url = self.driver.current_url
             self.try_count -= 1
+            self.exists_exception = True
+            print(f"ExecptionType: {exception_type}\n ARCHIVO: {filename}\n LINEA:{ line_number } \n ERROR: {exception_object}") 
 
-    # def initalize_bot(self,url):
-    #     try:
-            
-    #     except Exception as error:
-    #         self.url = self.driver.current_url
-    #         print("EXCEPT: initalize_bot()")
-    #         if self.exists_discounts == False:
-    #             self.email_send("ERROR UPS! BOT PRECIO CONTACTE EL ADMINISTRADOR  :C",self.email_content)
-    #         exception_type, exception_object, exception_traceback = sys.exc_info()
-    #         filename = exception_traceback.tb_frame.f_code.co_filename
-    #         line_number = exception_traceback.tb_lineno        
-    #         # self.driver.quit()
-    #         print(f"ExecptionType: {exception_type}\n ARCHIVO: {filename}\n LINEA:{ line_number } \n Object: {exception_object} \nERROR"  ,error)
-    
     def iniciar_bot(self,url): 
         try:
         
+
+    
+            
+            
             if len(self.url) == 0:
                 self.url = url
-
-            print("URL: ------> ",self.url)
-            print("TRY COUNT: -----> ", self.try_count)
-            
-            
             while True:
+                print("TRY COUNT: ",self.try_count,"\nURL: ",self.url )
+                self.exists_exception = False
                 self.driver.get(self.url)
 
                 time.sleep(3)
@@ -277,32 +276,52 @@ class Bot():
                 print("MAX PAGES: ",self.max_page_retry)
                 self.for_products_container(self.max_page_retry)
                 if self.try_count <= 0:
-                        break
+                    break
                 
             
             if self.exists_discounts:
 
-                self.driver.close()
-                session_id = self.driver.session_id
-                print("SESSION ",session_id)
+                self.driver.quit()
+                
                 # # end_timer = time.time()
                 # print("TIMER: ",end_timer - self.start_timer)
                 print("ENVIO DESCUENTOS")
-                self.email_send("BOT ERROES DE PRECIO :)",self.email_content)
+                category_name =self.url.split("/")[-1].split("?")[0].capitalize().replace("-"," ")
+                self.email_send(f"DESCUENTOS!! {category_name} ",self.email_content)
+            
+         
             
             print("TERMINO EL PROCESO")
-        except Exception as error:
-            self.url = self.driver.current_url
-            self.try_count -= 1
+        except Exception:
             exception_type, exception_object, exception_traceback = sys.exc_info()
             filename = exception_traceback.tb_frame.f_code.co_filename
             line_number = exception_traceback.tb_lineno        
-            print(f"ExecptionType: {exception_type}\n ARCHIVO: {filename}\n LINEA:{ line_number } \n Object: {exception_object} \nERROR"  ,error)
-    
+            print(f"ExecptionType: {exception_type}\n ARCHIVO: {filename}\n LINEA:{ line_number } \n ERROR: {exception_object}") 
+            self.url = self.driver.current_url
+            self.try_count -= 1
+            self.exists_exception = True
     
 
 
 
 if   __name__ == '__main__':
-    
-    Bot().iniciar_bot("https://www.falabella.com/falabella-cl/category/cat7450088/Calcetines")
+    man_category = [
+        'https://www.falabella.com/falabella-cl/category/cat1720006/Zapatos?isPLP=1',
+        'https://www.falabella.com/falabella-cl/category/cat5260002/Ropa-interior?isPLP=1',
+        'https://www.falabella.com/falabella-cl/category/cat1320008/Moda-Hombre?isPLP=1',
+        'https://www.falabella.com/falabella-cl/category/cat6930003/Ropa-deportiva-hombre?isPLP=1',
+        'https://www.falabella.com/falabella-cl/category/cat2036/Fitness?isPLP=1',
+        'https://www.falabella.com/falabella-cl/category/cat6050003/Accesorios-Hombre?isPLP=1',
+        'https://www.falabella.com/falabella-cl/category/cat7660002/Belleza?facetSelected=true&f.product.attribute.G%C3%A9nero=Hombre&isPLP=1',
+    ]
+    test_category = [
+        'https://www.falabella.com/falabella-cl/category/cat5260002/Ropa-interior-y-pijamas?isPLP=1',
+        'https://www.falabella.com/falabella-cl/category/cat5260002/Ropa-interior?isPLP=1'
+    ]
+    start = time.time()
+    for url in   man_category:
+        Bot().iniciar_bot(url)
+    finish = time.time()
+    print("TIEMPO: ------------------------------>",(finish - start)/60)
+
+        
